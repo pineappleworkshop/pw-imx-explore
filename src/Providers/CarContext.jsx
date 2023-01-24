28cddbeef35cd60054509d7140a3734bf316b78c
clarkpsmith <clark.smith79@gmail.com>
2023-01-24 11:44:23 -0700

    add speed

---
+++ src/Providers/CarContext.jsx
@@ -0,0 +1,23 @@
+import React, { createContext, useContext, useState } from "react";
+
+
+const CarDataContext = createContext(null);
+
+const useCardDataContext = () => useContext(CarDataContext);
+
+function CarDataProvider({ children }) {
+const [speed, setSpeed] = useState(0)
+  
+
+  return (
+    <CarDataContext.Provider
+      value={{
+       speed, 
+       setSpeed
+      }}>
+      {children}
+    </CarDataContext.Provider>
+  );
+}
+
+export { CarDataProvider, useCardDataContext };
